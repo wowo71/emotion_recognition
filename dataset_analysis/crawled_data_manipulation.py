c45adc104533742adcf5bb8ce94d2402d6630c15
wowo71 <w.bak95+github@gmail.com>
2021-09-05 21:48:51 +0200

    added cleaning functions for data clean up

--- dataset_analysis/crawled_data_manipulation.py
+++ dataset_analysis/crawled_data_manipulation.py
@@ -1,4 +1,5 @@
 from os import walk
+import re
 
 summarized_outputs = "/home/wojtek/Desktop/emotion_recognition/dataset/sumarized_outputs"
 filtered_outputs = "/home/wojtek/Desktop/emotion_recognition/dataset/filtered_outputs"
@@ -16,4 +17,21 @@ def remove_duplicates_from_files_from_dir(dataset_dir):
             f2.writelines(lines)
 
 
-remove_duplicates_from_files_from_dir(summarized_outputs)
+def remove_links_and_tagged_accounts(filtered_output_dir):
+    filenames = next(walk(filtered_output_dir), (None, None, []))[2]
+    for filename in filenames:
+        file_abs_path = filtered_output_dir + "/" + filename
+        with open(file_abs_path, "r") as f1:
+            text = f1.read()
+        cleared_file_abs_path = filtered_outputs + "/" + filename.split(".")[
+            0] + "_cleaned.txt"
+        with open(cleared_file_abs_path, "w") as f2:
+            text = re.sub(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", '', text, flags=re.MULTILINE)
+            text = (re.sub(r"[@](.*?)[\s]", '', text, flags=re.MULTILINE))
+            text = (re.sub(r"   ", ' ', text, flags=re.MULTILINE))
+            text = (re.sub(r"  ", ' ', text, flags=re.MULTILINE))
+            f2.write(re.sub(r"  ", ' ', text, flags=re.MULTILINE))
+
+
+# remove_duplicates_from_files_from_dir(summarized_outputs)
+remove_links_and_tagged_accounts(filtered_outputs)
